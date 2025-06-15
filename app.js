2767d10b64a74750f9928287a9ce7108f161c1e6
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-06-15 14:00:24 +0100

    feat(models): add User model with username, email, and password fields

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ dotenv.config();  // Load env vars first
 
 import express from "express";
 import connectDB from "./src/config/db.js";
+import eje from "ejs";
 
+// Initialize the Express application
 const app = express();
-
+// Set the port from environment variables or default to 5000
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
+// connect to the database
 connectDB();
 
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+// Serve static files from public
+app.use(express.static("./src/public"));
+
+// Define a simple route
 app.get("/", (req, res) => {
   res.send("HELLO WORLD and welcome to " + process.env.APP_NAME);
 });
 
+// Import and use the routes
 app.listen(PORT, () => {
-  console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
+  console.log(`server is running on http://localhost:${PORT}`);
 });
