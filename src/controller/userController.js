34b6a04a4827075d7dd8987e8ef552cbe0637c71
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-06-15 14:23:23 +0100

    feat(controller): add homepage controller to render home.ejs

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,1 +1,20 @@
-import User from  "../model/UserModel.js";
+import User from  "../model/UserModel.js"
+
+
+const getHomePage = async (req, res) => {
+    try {
+        res.status(200).render("index", {
+            title:"Home Page",
+        })
+    } catch (error) {
+     console.error("Error rendering homepage:", error.message);
+     res.status(500).error("error", {
+        title: "Error",
+        message: "Something went wrong while rendering the homepage."
+     })   
+    }
+}
+
+module.exports = {
+    getHomePage
+}
